f5347ed13c5e7274de2cecff95d2205420f948fc
carey <980328708@qq.com>
2024-11-30 15:45:44 +0800

    basic connection functions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # python-adb-ocr
 Paddle-ADB-OCR 是一个基于 Python 和 PaddleOCR 的 ADB（Android Debug Bridge）API 封装库，专注于移动设备上的文字识别。通过结合 ADB 的强大功能和 PaddleOCR 的高精度 OCR 技术，本库提供了一系列简单易用的接口，帮助开发者快速实现移动设备上的文字识别任务以及自动化任务
+# 主要特点
+**跨平台支持**：
+基于 ADB，支持 Windows、macOS 和 Linux 平台。
+
+**高精度 OCR**：利用 PaddleOCR 的强大文字识别能力，提供高精度的文字识别结果。
+
+**简单易用**：封装了一系列简洁的 API，方便开发者快速集成到项目中。
+
+**多设备管理**：支持同时管理多个 Android 设备，方便进行批量操作。
+
+**截图与识别一体化**：提供一键截图并识别的功能，简化操作流程。
+
---
+++ adb.py
@@ -0,0 +1,118 @@
+import psutil
+import asyncio
+
+from loguru import logger
+from paddleocr import PaddleOCR
+from adb_shell.adb_device import AdbDeviceTcp
+from adb_shell.adb_device_async import AdbDeviceTcpAsync
+
+
+class AdbOCR:
+    def __init__(self):
+        self._device: AdbDeviceTcpAsync|None = None
+        self.ocr_engine = PaddleOCR()
+
+    async def load(self, port: int, host: str = "localhost", scan_if_fail=True) -> None:
+        """
+        Load the ADB connection.
+
+        Args:
+            port: The port to connect to.
+            host: The host to connect to.
+            scan_if_fail: Whether to scan for local devices if the connection fails.
+
+        Returns:
+        """
+        task1 = asyncio.create_task(self._init_ocr_engine())
+        task2 = asyncio.create_task(self._connect_device(port=port, host=host, scan_if_fail=scan_if_fail))
+
+        await task1
+        await task2
+
+    async def _connect_device(self, port: int, host: str = "localhost", scan_if_fail: bool = True) -> None:
+        """
+        Connect to the device.
+
+        Args:
+            port: The port to connect to.
+            host: The host to connect to.
+            scan_if_fail: Whether to scan for devices if the connection fails.
+
+        Returns:
+        """
+        if self._device is not None:
+            return
+
+        device = AdbDeviceTcpAsync(host=host, port=port, default_transport_timeout_s=9)
+        logger.debug(f"Connecting to Device[{host}:{port}]...")
+
+        try:
+            connection = await device.connect()
+            if connection:
+                self._device = device
+                return
+        except OSError:
+            logger.warning(f"[X]Failed to connect to Device[{host}:{port}].")
+
+        # scan for devices at localhost if the connection failed
+        if host == "localhost" and scan_if_fail:
+            # scan the local devices
+            port_found = await self._scan_local_devices()
+
+            if port_found:
+                try:
+                    connection = await self._connect_device(port=port_found, host=host, scan_if_fail=False)
+                    if connection:
+                        self._device = device
+                        return
+                except OSError:
+                    pass
+
+        logger.warning(
+            f"Failed to connect to emulator, please make sure the ADB is enabled on your emulator.")
+
+    async def _scan_local_devices(self) -> int | None:
+        """
+        Scan for devices at localhost.
+
+        Returns: the open adb ports or None if no device was found.
+        """
+        logger.info("Scanning for open adb devices at localhost...")
+
+        connections = [
+            conn for conn in psutil.net_connections("tcp4") if conn.laddr.port >= 5555 and conn.status == "LISTEN"
+        ]
+
+        logger.warning(f"There are {len(connections)} open ports, trying to find the abd port.")
+
+        for conn in connections:
+            logger.debug(f"Scanning port {conn.laddr.port} for ADB...")
+            try:
+                adb_device = AdbDeviceTcp("localhost", port=conn.laddr.port, default_transport_timeout_s=0.5)
+                if adb_device.connect(read_timeout_s=0.5):
+                    adb_device.close()
+                    return conn.laddr.port
+            # Reason for disable: The code above can throw a lot of different exceptions, this is the simplest solution.
+            except Exception as e:  # pylint: disable=broad-exception-caught
+                logger.debug(f"Port {conn.laddr.port} threw '{e}'.")
+
+        logger.warning("No local device was found. Make sure ADB is enabled in your emulator's settings.")
+        return None
+
+    async def _init_ocr_engine(self) -> None:
+        """
+        Initialize the OCR engine.
+
+        Returns:
+        """
+        self.ocr_engine = PaddleOCR(lang='ch')
+
+    def is_connected(self) -> bool:
+        """
+        Check if the device is connected.
+
+        Returns:
+        """
+        return self._device and self._device.available
+
+
